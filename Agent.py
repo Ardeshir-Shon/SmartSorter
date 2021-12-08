0a0580f1a93bbeda8d391e2d9c6ea4d4ad646547
Todd <40245048+tcharte@users.noreply.github.com>
2021-12-07 16:59:25 -0800

    update agent
    minor updates and added network model (pytorch)

--- Agent.py
+++ Agent.py
@@ -4,6 +4,9 @@ from Buffer import Buffer
 from Pallet import Pallet
 
 from collections import defaultdict
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 ## action features function
 ## normalization of the features
@@ -20,6 +23,7 @@ class Agent():
         self.method = method
         # self.qvalues = {}
         self.stateValues = defaultdict(0)
+        actions = (self.belt.capacity+1)*(self.buffer.length*self.buffer.width+1)*[0]
     
     def doAction(self):
         print("choose action and get reward!")
@@ -29,7 +33,7 @@ class Agent():
     
     
     def getStateFeatures(self):
-        
+        print('get all state features!')
     
     def getStateFeatures(self):
         features = []
@@ -63,6 +67,10 @@ class Agent():
     def getBestAction(self,stateFeatures,model):
         print("should calculate best value based on given model and stateFeatures by examining different actions")
     
+    def getPossibleActions(self):
+        # Use logic arrays ... ie: [self.belt.slots>0] [self.buffer.slots==0]
+        print("get possible actions!")
+
     def moveBufferToPallet(self,sourceX:int,sourceY:int):
         print("move!")
     
@@ -77,3 +85,19 @@ class Agent():
     
     def interpretState(self,stateCode:tuple):
         return list(stateCode)
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.linear(self.fc1(x))
+        x = F.linear(self.fc2(x))
+        x = self.fc3(x)
+        return x
